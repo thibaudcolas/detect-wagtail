3c0312ab629a3e319468fb59b47c2dc8fc323376
Thibaud Colas <thibaudcolas@gmail.com>
2020-11-18 11:45:13 +0000

    Refine packaging of repository

--- src/detect-wagtail.js
+++ src/detect-wagtail.js
@@ -1,14 +1,8 @@
-// Use https://regexper.com/.
-// Using an arbitrary limit of 5 digits per image dimension.
-// Using a very conservative 3 digits for percentages.
+// Use https://regexper.com/ to visualize the regular expressions.
+// Notes on implementation:
+// - Specifying file extensions doesn’t refine the results meaningfully enough.
+// - Creating exact matches for the renditions suffixes isn’t really that useful in real-world usage (for example limiting the length of sequences of digits).
 const renditions = {
-  v1: /\.(fill-\d{1,5}x\d{1,5}(-c\d{1,3})?|(max|min)-\d{1,5}x\d{1,5}|(width|height)-\d{1,5}|scale-\d{1,3})(\.format-(jpeg|png|gif|webp))?(\.(jpegquality|webpquality)-\d{1,3})?\.(jpg|jpeg|png|gif|webp|avif)/,
-  v2: /\/images\/[\w-.]+\.(fill-\d{1,5}x\d{1,5}(-c\d{1,3})?|(max|min)-\d{1,5}x\d{1,5}|(width|height)-\d{1,5}|scale-\d{1,3})(\.format-(jpeg|png|gif|webp))?(\.(jpegquality|webpquality)-\d{1,3})?\.(jpg|jpeg|png|gif|webp|avif)/,
-  v3: /(\/images\/[\w-.]+\.(fill-\d{1,5}x\d{1,5}(-c\d{1,3})?|(max|min)-\d{1,5}x\d{1,5}|(width|height)-\d{1,5}|scale-\d{1,3})(\.format-(jpeg|png|gif|webp))?(\.(jpegquality|webpquality)-\d{1,3})?|\/media\/original_images\/[\w-.]+)\.(jpg|jpeg|png|gif|webp|avif)/,
-  v4: /(\/images\/[\w-.]+\.(fill-\d{1,5}x\d{1,5}(-c\d{1,3})?|(max|min)-\d{1,5}x\d{1,5}|(width|height)-\d{1,5}|scale-\d{1,3})(\.format-(jpeg|png|gif|webp))?(\.(jpegquality|webpquality)-\d{1,3})?|\/media\/original_images\/[\w-.]+|\/media\/images\/[\w-.]+\.original)\.(jpg|jpeg|png|gif|webp|avif|JPG|JPEG|PNG|GIF|WEBP|AVIF)/,
-  v5: /(\/images\/[\w-.]+\.((fill|max|min)-\d+x\d+(-c\d{1,3})?|(width|height)-\d+|scale-\d{1,3})(\.format-(jpeg|png|webp))?(\.(jpeg|webp)quality-\d{1,3})?|\/media\/original_images\/[\w-.]+|\/media\/images\/[\w-.]+\.original)\./,
-  v6: /(\/images\/[\w-.]+\.((fill|max|min)-\d+x\d|(width|height|scale)-\d)|\/media\/images\/[\w-.]+\.original|\/media\/original_images\/)/,
-  v7: /(\.[a-z]+|\/media)(\/[\w-]+)?\/(original_images\/[\w-]+\.|images\/[\w-.]+\.((fill|max|min)-\d+x\d|(width|height|scale)-\d|original))/,
   // Optimized for real-world relevance, with as few false positives as possible, but still some.
   strict: /(\.[a-z]+|\/media)(\/[\w-]+)?\/(original_images\/[\w-]+\.|images\/[\w-.]+\.((fill|max|min|width|height|scale)-\d|original))/,
   less_strict_but_long: /((\.[a-z]+|\/media)(\/[\w-]+)?\/(images\/[\w-.]+\.original|original_images\/[\w-]+\.)|\/images\/[\w-.]+\.(fill|max|min|width|height|scale)-\d)/,
@@ -29,4 +23,5 @@ const detectWagtail = (html) => {
 
 module.exports = {
   detectWagtail,
+  renditions,
 };
