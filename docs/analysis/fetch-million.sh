90510951f0d1d5ba113bace6d30be8ced497c3e5
Thibaud Colas <thibaudcolas@gmail.com>
2024-01-01 17:09:37 +0000

    Update curl parameters

--- docs/analysis/fetch-million.sh
+++ docs/analysis/fetch-million.sh
@@ -14,5 +14,5 @@ do
   file="${file/https\:\/\//}"
   file="${file/\//}"
   site="https://$file"
-  curl -sL --max-time 15 --connect-timeout 5 --speed-time 15 --speed-limit 1000 "$site" > "$2/$file.html"
+  curl --user-agent "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15" -sL --max-time 15 --connect-timeout 5 --speed-time 15 --speed-limit 1000 "$site" > "$2/$file.html"
 done < "./$1"
